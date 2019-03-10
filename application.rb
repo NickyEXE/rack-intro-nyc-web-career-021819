79be8d3714c5b056b2023c6188260788d87a8d7b
NickyEXE <dovern42@gmail.com>
2019-03-10 19:05:10 -0400

    done

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Nicky Dover, I am a destroyer of worlds"
     resp.finish
   end
 
 end
-
